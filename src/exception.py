86559d5c274a59e26bfdbc13580a5ef7b6d3895c
MamunurRahmanMoon <mamunurrahmanmoon@gmail.com>
2025-04-05 03:01:13 +0600

    Logger, Exception handler added

---
+++ src/exception.py
@@ -0,0 +1,32 @@
+import sys
+from src.logger import logging
+
+def error_message_details(error, error_details:sys):
+    """
+    This function takes an error and its details as input and returns a formatted error message.
+    """
+    _, _, exc_tb = error_details.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] error message: [{str(error)}]"
+    return error_message
+
+class CustomException(Exception):
+    """
+    Custom exception class that inherits from the built-in Exception class.
+    It takes an error message and its details as input and formats it using the error_message_details function.
+    """
+    def __init__(self, error_message, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_details=error_details)
+
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
+    
---
+++ src/logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import sys
+
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
+LOG_PATH = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(LOG_PATH, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_PATH, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s]: %(lineno)d %(name)s %(levelname)s: %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+if __name__ == "__main__":
+    try:
+        logging.info("Logging is set up.")
+        logging.info("This is an info message.")
+        logging.warning("This is a warning message.")
+        logging.error("This is an error message.")
+        logging.critical("This is a critical message.")
+        # Example of an exception
+        1 / 0
+    except Exception as e:
+        logging.exception("An exception occurred")
